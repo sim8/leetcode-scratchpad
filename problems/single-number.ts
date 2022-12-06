4476fe6198f8abb697851a9b9870b244374da5c6
Simeon Lees <simeon.k.lees@gmail.com>
2022-12-06 13:02:56 +0000

    Complete solution for singleNumber

--- problems/single-number.ts
+++ problems/single-number.ts
@@ -3,12 +3,28 @@ import { buildTests } from '../utils/buildTests';
 // https://leetcode.com/problems/single-number/
 
 function singleNumber(nums: number[]): number {
-  return 4;
+  const singleNumbersSoFar = {};
+  nums.forEach((num) => {
+    if (num in singleNumbersSoFar) {
+      delete singleNumbersSoFar[num];
+    } else {
+      singleNumbersSoFar[num] = true;
+    }
+  });
+  return parseInt(Object.keys(singleNumbersSoFar)[0], 10);
 }
 
 buildTests('singleNumber', singleNumber, [
   {
-    inputs: [[3]],
-    output: 2,
+    inputs: [[2, 2, 1]],
+    output: 1,
+  },
+  {
+    inputs: [[4, 1, 2, 1, 2]],
+    output: 4,
+  },
+  {
+    inputs: [[1]],
+    output: 1,
   },
 ]);
