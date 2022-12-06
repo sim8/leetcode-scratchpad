85af6c3317a5c727187e7cc4c22e06d462bd3ea7
Simeon Lees <simeon.k.lees@gmail.com>
2022-12-06 23:52:33 +0000

    Complete add two numbers

--- problems/add-two-numbers.ts
+++ problems/add-two-numbers.ts
@@ -7,8 +7,35 @@ type ListNode = {
   next: ListNode | null;
 };
 
+function listToNumber(listNode: ListNode): number {
+  let currentNode: ListNode | null = listNode;
+  let radix = 1;
+  let acc = 0;
+
+  while (currentNode) {
+    acc += currentNode.val * radix;
+
+    currentNode = currentNode.next;
+    radix *= 10;
+  }
+
+  return acc;
+}
+
 function addTwoNumbers(l1: ListNode, l2: ListNode): ListNode {
-  return l1;
+  const num1 = listToNumber(l1);
+  const num2 = listToNumber(l2);
+
+  const result = num1 + num2;
+
+  const valueStrs = `${result}`.split('');
+
+  const finalResult = valueStrs.reduce<ListNode>(
+    (acc, current) => ({ val: parseInt(current, 10), next: acc }),
+    null as any
+  );
+
+  return finalResult;
 }
 
 function buildLinkedList(values: number[]): ListNode {
@@ -22,8 +49,8 @@ buildTests({
   algorithm: addTwoNumbers,
   testCases: [
     {
-      inputs: [buildLinkedList([243]), buildLinkedList([564])],
-      output: buildLinkedList([708]),
+      inputs: [buildLinkedList([2, 4, 3]), buildLinkedList([5, 6, 4])],
+      output: buildLinkedList([7, 0, 8]),
     },
   ],
   serializeResult: (listNode) => JSON.stringify(listNode),
