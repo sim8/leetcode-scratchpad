f99755c6d32cb35c0ce2e955d8f3d013501f01ed
Simeon Lees <simeon.k.lees@gmail.com>
2022-12-30 23:59:10 +0000

    Letter combinations implementation

--- problems/letter-combinations-of-a-phone-number.ts
+++ problems/letter-combinations-of-a-phone-number.ts
@@ -2,7 +2,37 @@ import { buildTests } from '../utils/buildTests';
 
 // https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
-function letterCombinations(digits: string): string[] {}
+const NUMBERS_TO_LETTERS: Record<string, string[]> = {
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z'],
+};
+
+function letterCombinations(digits: string): string[] {
+  if (!digits.length) {
+    return [];
+  }
+  const permutationsOfCurrentDigit = NUMBERS_TO_LETTERS[digits[0]];
+
+  if (digits.length === 1) {
+    return permutationsOfCurrentDigit;
+  }
+
+  const permutationsForRestOfString = letterCombinations(digits.slice(1));
+
+  return permutationsOfCurrentDigit
+    .map((letter) =>
+      permutationsForRestOfString.map((restOfString) =>
+        letter.concat(restOfString)
+      )
+    )
+    .flat();
+}
 
 buildTests({
   algorithm: letterCombinations,
